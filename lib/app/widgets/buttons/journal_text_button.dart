4ddcbe7e0ec63c4103a9d888369f6c0f7d39285e
Jay <sanjaysheombar@gmail.com>
2024-02-13 12:49:57 +0100

    Dart format

--- lib/app/widgets/buttons/journal_text_button.dart
+++ lib/app/widgets/buttons/journal_text_button.dart
@@ -2,8 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:journal/core/theme/typography.dart';
 
-
-
 class JournalTextButton extends StatelessWidget {
   const JournalTextButton({
     super.key,
