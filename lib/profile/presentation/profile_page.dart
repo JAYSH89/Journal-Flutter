020ae6908b5c85049b65cc7d8187b937482c8d26
Jay <sanjaysheombar@gmail.com>
2024-02-14 22:29:21 +0100

    placeholders for profile

--- lib/profile/presentation/profile_page.dart
+++ lib/profile/presentation/profile_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:journal/app/widgets/buttons/journal_text_button.dart';
 import 'package:journal/app/widgets/journal_app_bar.dart';
-import 'package:journal/app/widgets/journal_button.dart';
 import 'package:journal/core/theme/typography.dart';
 
 class ProfilePage extends StatelessWidget {
@@ -32,19 +32,55 @@ class ProfileView extends StatelessWidget {
 
     return Scaffold(
       appBar: JournalMaterialAppBar(titleLabel: _title),
-      body: Center(child: _ProfileViewContent()),
+      body: _ProfileViewContent(),
     );
   }
 }
 
 class _ProfileViewContent extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
+  Widget build(BuildContext context) => SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _header(),
+            const SizedBox(height: 8),
+            _weightChart(),
+            const SizedBox(height: 8),
+            _developerSettingsButton(() => context.go("/profile/developer")),
+          ],
+        ),
+      );
+
+  // TODO
+  Widget _header() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text("Hello, NAME", style: satoshiBlack.copyWith(fontSize: 28)),
+        const Spacer(),
+        const SizedBox(width: 48, height: 48, child: Placeholder()),
+      ],
+    );
+  }
+
+  Widget _weightChart() => const SizedBox(
+        height: 200,
+        width: double.infinity,
+        child: Placeholder(),
+      );
+
+  Widget _developerSettingsButton(Function() onPressed) => Row(
         children: [
-          Center(child: Text("Hello, Profile", style: satoshiRegular)),
-          JournalButton(onPressed: () => context.go("/profile/developer")),
+          Expanded(
+            child: JournalTextButton(
+              text: "To developer settings",
+              onPressed: onPressed,
+            ),
+          ),
         ],
       );
 }
